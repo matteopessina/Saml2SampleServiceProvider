61690ff063ead34937cf56eb4b372ed2c16b3428
Pessina Matteo <matteo.pessina@reti.it>
2018-04-04 17:39:12 +0200

    a working sample with sustainsys.saml2 idp

--- Saml2SampleServiceProvider/Controllers/HomeController.cs
+++ Saml2SampleServiceProvider/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Web;
 using System.Web.Mvc;
 
@@ -14,5 +15,19 @@ namespace Saml2SampleServiceProvider.Controllers
 
             return View();
         }
+
+        [Authorize]
+        public ActionResult Secure()
+        {
+            var identity = System.Web.HttpContext.Current.User.Identity as ClaimsIdentity;
+            return View(identity.Claims);
+        }
+
+        [Authorize(Roles ="Administrator")]
+        public ActionResult AdminOnly()
+        {
+            return View();
+        }
+
     }
 }
